5cfac4ba398fb22f2191683ac6f6b845dbe6e66a
Nuno Louro <nlouro@student.42heilbronn.de>
2021-09-30 12:55:07 +0200

    ft_printf WIP

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: nuno <nlouro@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/25 23:43:32 by nuno              #+#    #+#             */
-/*   Updated: 2021/09/30 12:31:48 by nlouro           ###   ########.fr       */
+/*   Updated: 2021/09/30 12:54:31 by nlouro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,32 +24,34 @@
  * %% print a percent sign.
  */
 
+static int	ft_putstr_fd(char *s, int fd);
+//static int	ft_putchar_fd(char c, int fd);
+static int	ft_putnbr_fd(int n, int fd);
+
 static	int	print_arg(const char *fmt, va_list *ap, int i)
 {
 	int		d;
 	int		len;
 	char	c;
-    char	*s;
+	char	*s;
 
 	len = 0;
-	switch(fmt[i]) {
-		case 's':
-			s = va_arg(*ap, char *);
-			ft_putstr_fd(s, 1);
-			len = 4;
-			break;
-		case 'd':
-			d = va_arg(*ap, int);
-			ft_putnbr_fd(d, 1);
-			len = 1;
-			break;
-		case 'c':
-			c = va_arg(*ap, int);
-			ft_putchar_fd(c, 1);
-			len = 1;
-			break;
+	if (fmt[i] == 's')
+	{
+		s = va_arg(*ap, char *);
+		len = ft_putstr_fd(s, 1);
 	}
-
+	else if (fmt[i] == 'd')
+	{
+		d = va_arg(*ap, int);
+		len = ft_putnbr_fd(d, 1);
+	}
+	else if (fmt[i] == 'c')
+	{
+		c = va_arg(*ap, int);
+		write(1, &c, 1);
+		len = 1;
+	}
 	return (len);
 }
 
@@ -84,4 +86,44 @@ int	ft_printf(const char *fmt, ...)
 	}
 	va_end(ap);
 	return (0);
+}
+
+static int	ft_putstr_fd(char *s, int fd)
+{
+	size_t	i;
+
+	if (s == 0)
+		return (0);
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	write(fd, s, i);
+	return (i);
+}
+
+static	int	ft_putnbr_fd(int n, int fd)
+{
+	unsigned int	nb;
+	int i;
+
+	nb = n;
+	i = 0;
+	if (n < 0)
+	{
+		write(fd, "-", 1);
+		nb = -n;
+		i++;
+	}
+	if (nb >= 10)
+	{
+		ft_putnbr_fd(nb / 10, fd);
+		nb = nb % 10;
+	}
+	if (nb < 10)
+	{
+		nb = nb + 48;
+		write(fd, &nb, 1);
+		i++;
+	}
+	return (i);
 }
